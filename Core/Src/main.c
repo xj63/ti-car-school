507c2e0254b62081000c9f92b3f9a9d2fcf653fd
zl <2293721550@qq.com>
2024-04-17 15:51:31 +0800

    buf

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -106,6 +106,7 @@ int main(void)
     MX_USART3_UART_Init();
     MX_TIM5_Init();
     /* USER CODE BEGIN 2 */
+    motor_init();
     HAL_TIM_Base_Start_IT(&htim5);
     /* USER CODE END 2 */
     start_turn(45);
