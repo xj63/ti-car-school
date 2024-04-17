2fb24f965d1e8af462b66b4bbb9c47ae835749ac
zl <2293721550@qq.com>
2024-04-17 23:45:47 +0800

    XZ

--- Core/user/motor_control.h
+++ Core/user/motor_control.h
@@ -2,7 +2,7 @@
  * @Author: zl 2293721550@qq.com
  * @Date: 2024-04-03 23:09:00
  * @LastEditors: zl 2293721550@qq.com
- * @LastEditTime: 2024-04-07 19:05:53
+ * @LastEditTime: 2024-04-17 16:53:28
  * @FilePath: \DaChuang\Core\user\motor_control.h
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,8 +11,8 @@
 
 #include "main.h"
 
-#define TURN_SPEED 30
-#define STRAIGHT_SPEED 30
+#define TURN_SPEED 10
+#define STRAIGHT_SPEED 10
 
 extern int8_t left_tar;
 extern int8_t right_tar;
