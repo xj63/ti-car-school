ba183792e7a69ba8b16e97a43f61bb0ffdb73c11
xj63 <xj63work@163.com>
2024-04-28 18:36:23 +0800

    fix(turn): variable name repeat.

--- Core/user/turn_abs.c
+++ Core/user/turn_abs.c
@@ -24,15 +24,15 @@
 #include "stdlib.h"
 #include "main.h"
 
-int8_t turn_cnt = 0;
-float angle = 0;
-int8_t max_speed = 5;
+int8_t turn_abs_cnt = 0;
+float turn_abs_angle = 0;
+int8_t turn_abs_max_speed = 5;
 
-struct PID *turn_pid;
-float origin;
-float move_line_angle;
-static struct pid_data turn_head_data;
-struct PID *turn_XZ_pid = NULL;
+struct PID *turn_abs_pid;
+float turn_abs_origin;
+float turn_abs_move_line_angle;
+static struct pid_data turn_abs_head_data;
+struct PID *turn_abs_XZ_pid = NULL;
 
 /**
  * 初始化转向PID结构体
@@ -41,8 +41,8 @@ struct PID *turn_XZ_pid = NULL;
 // 初始化转向 得到原始角度 设置pid的值
 void turn_abs_head_init()
 {
-    origin = Get_gyr_value(gyr_z_yaw) + 180;
-    pid_init(&turn_head_data, 0, 0.6, 0, 0, 5);
+    turn_abs_origin = Get_gyr_value(gyr_z_yaw) + 180;
+    pid_init(&turn_abs_head_data, 0, 0.6, 0, 0, 5);
 }
 
 void turn_abs()
@@ -58,21 +58,21 @@ void turn_abs()
         int16_t diff_L = left_L - right_L;
         // int16_t turn_XZ = returnPID(turn_XZ_pid, diff_L, 0);
         int16_t turn_XZ = 0;
-        diff = turn_head_diff(angle, origin);
-        int turn_speed = pid_inc(&turn_head_data, -diff);
+        diff = turn_head_diff(turn_abs_angle, turn_abs_origin);
+        int turn_speed = pid_inc(&turn_abs_head_data, -diff);
         if ((diff <= 2) && (diff >= -2))
         {
-            turn_cnt--;
-            if (turn_cnt == 0)
+            turn_abs_cnt--;
+            if (turn_abs_cnt == 0)
             {
                 turn_flag = 0;
-                angle = 0;
+                turn_abs_angle = 0;
                 left_tar = 0;
                 right_tar = 0;
                 left_L = 0;
                 right_L = 0;
-                free(turn_XZ_pid);
-                turn_XZ_pid = NULL;
+                free(turn_abs_XZ_pid);
+                turn_abs_XZ_pid = NULL;
                 return;
             }
         }
@@ -92,8 +92,8 @@ void turn_abs_start_turn(float turn_angle)
     while (moving_flag)
         ;
     turn_flag = 1;
-    angle = turn_angle;
-    turn_cnt = 1;
-    turn_XZ_pid = initPID(0.1, 0, 0, 1, 10);
+    turn_abs_angle = turn_angle;
+    turn_abs_cnt = 1;
+    turn_abs_XZ_pid = initPID(0.1, 0, 0, 1, 10);
     moving_flag = turn_flag | straight_flag;
 }
