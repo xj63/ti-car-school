05435a5ef547dd9c49586f7fd01290e15d456ed0
zl <2293721550@qq.com>
2024-04-29 14:06:41 +0800

    fix: untrack MDK-ARM.

--- MDK-ARM/DaChuang/pid.d
+++
@@ -1,4 +0,0 @@
-dachuang/pid.o: ..\Core\user\pid.c \
-  D:\program\keil_5\ARM\ARMCLANG\Bin\..\include\stdlib.h \
-  D:\program\keil_5\ARM\ARMCLANG\Bin\..\include\stdio.h \
-  ..\Core\user\printTo.h
--- MDK-ARM/DaChuang/pid_yuan.d
+++
@@ -1,1 +0,0 @@
-dachuang/pid_yuan.o: ..\Core\user\pid_yuan.c ..\Core\user\pid_yuan.h
