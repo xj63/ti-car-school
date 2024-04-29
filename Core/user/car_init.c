447310d3c6c6ad290532f9b06d58f10678b3f90d
xj63 <xj63work@163.com>
2024-04-29 15:13:09 +0800

    style: format car_init car_run

--- Core/user/car_init.c
+++ Core/user/car_init.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include "tim.h"
-#include "straight.h"
 #include "motor.h"
+#include "straight.h"
+#include "tim.h"
 #include "turn_abs.h"
 
-void car_init()
-{
-    motor_init();
-    turn_abs_head_init();
-    HAL_TIM_Base_Start_IT(&htim5);
+void car_init() {
+  motor_init();
+  turn_abs_head_init();
+  HAL_TIM_Base_Start_IT(&htim5);
 }
