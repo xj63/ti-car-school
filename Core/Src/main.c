2b9311548f98fac63d1fd8b8e46054587b0dff24
zl <2293721550@qq.com>
2024-04-29 13:46:31 +0800

    子林的小修改

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -132,13 +132,13 @@ int main(void)
     /* Infinite loop */
     /* USER CODE BEGIN WHILE */
     start_straight(2000);
-		start_keep_angle(0);
-		turn_abs_start_turn(90);
-		start_straight(2000);
-		start_keep_angle(90);
-		turn_abs_start_turn(40);
-		start_straight(2000);
-		start_keep_angle(40);
+    start_keep_angle(0);
+    turn_abs_start_turn(90);
+    start_straight(2000);
+    start_keep_angle(90);
+    turn_abs_start_turn(40);
+    start_straight(2000);
+    start_keep_angle(40);
 
     while (1)
     {
