ac85a0bae0c1eb5573e9ced94fd1388beec388ff
xj63 <xj63work@163.com>
2024-04-30 17:48:21 +0800

    fix: ring times

--- Core/user/drive/buzzer.c
+++ Core/user/drive/buzzer.c
@@ -10,7 +10,7 @@ void buzzer_ring(uint8_t len) {
   HAL_Delay(100);
   HAL_GPIO_WritePin(BUZZER_GPIO_Port, BUZZER_Pin, 1);
 
-  if (len != 0) {
+  if (len > 1) {
     HAL_Delay(100);
     buzzer_ring(len - 1);
   }
