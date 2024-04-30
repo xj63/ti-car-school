8ac026b025b0be3d14f36986a366dec8561afc74
zl <2293721550@qq.com>
2024-04-30 23:04:04 +0800

    fix: init gy901 at buttom.

--- Core/user/car_run.c
+++ Core/user/car_run.c
@@ -12,6 +12,7 @@ uint8_t sta = 0xff;
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
     HAL_UART_Receive_IT(&huart3, &sta, 1);
+	  turn_abs_head_init();
 	printTo(uart4, "%hhu\r\n", sta);
 }
 
