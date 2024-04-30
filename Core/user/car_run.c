cf48515d7a1961d2ff6bd658ebb157ade9ab71b7
zl <2293721550@qq.com>
2024-04-30 17:47:38 +0800

    feat: go stop p1

--- Core/user/car_run.c
+++ Core/user/car_run.c
@@ -17,7 +17,7 @@ void delay(uint16_t S)
 
 void stop_ring()
 {
-    buzzer_ring(2);
+    buzzer_ring(3);
     delay(5);
     buzzer_ring(2);
 }
@@ -65,11 +65,20 @@ void go_stop_p1()
     start_straight(2450);
     start_keep_angle(0.0);
     turn_abs_start_turn(45.0);
-    start_straight(1700);
+    start_straight(1200);
     start_keep_angle(45.0);
 
+    turn_abs_start_turn(90.0);
+    start_straight(1700);
+    start_keep_angle(90.0);
+
+    stop_ring();
+
+    start_straight(-1400);
+    start_keep_angle(90.0);
+
     turn_abs_start_turn(0.0);
-    start_straight(3200);
+    start_straight(3600);
     start_keep_angle(0.0);
 
     turn_abs_start_turn(45.0);
@@ -77,12 +86,7 @@ void go_stop_p1()
     start_keep_angle(45.0);
 
     turn_abs_start_turn(90.0);
-
-    start_straight(-1900);
-
-    stop_ring();
-
-    start_straight(5500);
+    start_straight(3400);
     start_keep_angle(90.0);
 
     turn_abs_start_turn(135.0);
@@ -90,11 +94,11 @@ void go_stop_p1()
     start_keep_angle(135.0);
 
     turn_abs_start_turn(180.0);
-    start_straight(2850);
+    start_straight(2900);
     start_keep_angle(180.0);
 
     turn_abs_start_turn(135.0);
-    start_straight(1700);
+    start_straight(1600);
     start_keep_angle(135.0);
 
     turn_abs_start_turn(180.0);
@@ -206,5 +210,5 @@ void mode2()
 
 void car_run()
 {
-    mode2();
+    go_stop_p1();
 }
