fbcffab816c798d963cf64860293ec3ac1dcca31
xj63 <xj63work@163.com>
2024-04-30 17:51:47 +0800

    feat: stop p3 todo.

--- Core/user/car_run.c
+++ Core/user/car_run.c
@@ -61,7 +61,6 @@ void go_start_to_end()
 
 void go_stop_p1()
 {
-    // TODO:
     start_straight(2450);
     start_keep_angle(0.0);
     turn_abs_start_turn(45.0);
@@ -114,6 +113,54 @@ void go_stop_p2()
 void go_stop_p3()
 {
     // TODO:
+    start_straight(2450);
+    start_keep_angle(0.0);
+
+    turn_abs_start_turn(45.0);
+    start_straight(1500);
+    start_keep_angle(45.0);
+
+    turn_abs_start_turn(0.0);
+    start_straight(500);
+    start_keep_angle(0.0);
+
+    // TODO: stop p3
+    {
+        turn_abs_start_turn(90.0);
+        start_straight(1200);
+        start_keep_angle(90.0);
+
+        start_straight(-1200);
+        start_keep_angle(90.0);
+    }
+
+    turn_abs_start_turn(0.0);
+    start_straight(3100);
+    start_keep_angle(0.0);
+
+    turn_abs_start_turn(45.0);
+    start_straight(700);
+    start_keep_angle(45.0);
+
+    turn_abs_start_turn(90.0);
+    start_straight(3400);
+    start_keep_angle(90.0);
+
+    turn_abs_start_turn(135.0);
+    start_straight(700);
+    start_keep_angle(135.0);
+
+    turn_abs_start_turn(180.0);
+    start_straight(2900);
+    start_keep_angle(180.0);
+
+    turn_abs_start_turn(135.0);
+    start_straight(1600);
+    start_keep_angle(135.0);
+
+    turn_abs_start_turn(180.0);
+    start_straight(2000);
+    start_keep_angle(180.0);
 }
 
 void go_stop_p4()
