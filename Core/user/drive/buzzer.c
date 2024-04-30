a4df705356493f7fe32026352ee2933e3928728a
xj63 <xj63work@163.com>
2024-04-30 16:32:06 +0800

    fix: ring after stop wait stop.

--- Core/user/drive/buzzer.c
+++ Core/user/drive/buzzer.c
@@ -2,9 +2,13 @@
 #include "main.h"
 
 void buzzer_ring(uint8_t len) {
+  while (get_flag())
+    continue;
+
   HAL_GPIO_WritePin(BUZZER_GPIO_Port, BUZZER_Pin, 0);
   HAL_Delay(100);
   HAL_GPIO_WritePin(BUZZER_GPIO_Port, BUZZER_Pin, 1);
+
   if (len != 0) {
     HAL_Delay(100);
     buzzer_ring(len - 1);
