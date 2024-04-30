53ddb181de230920051d6dc4d7b9a63151da2ec6
zl <2293721550@qq.com>
2024-04-30 20:48:53 +0800

    feat: stop p9 ok

--- Core/user/car_run.c
+++ Core/user/car_run.c
@@ -553,22 +553,22 @@ void go_stop_p10()
     start_keep_angle(135.0);
 
     turn_abs_start_turn(180.0);
-    start_straight(1400);
+    start_straight(2170);
     start_keep_angle(180.0);
 
     {
         turn_abs_start_turn(270.0);
-        start_straight(1400);
+        start_straight(1300);
         start_keep_angle(270.0);
 
         stop_ring();
 
-        start_straight(-1400);
+        start_straight(-1300);
         start_keep_angle(270.0);
     }
 
     turn_abs_start_turn(180.0);
-    start_straight(1500);
+    start_straight(930);
     start_keep_angle(180.0);
 
     turn_abs_start_turn(135.0);
@@ -608,7 +608,7 @@ void go_stop_p11()
     start_keep_angle(135.0);
 
     turn_abs_start_turn(180.0);
-    start_straight(1400);
+    start_straight(2100);
     start_keep_angle(180.0);
 
     {
@@ -623,7 +623,7 @@ void go_stop_p11()
     }
 
     turn_abs_start_turn(180.0);
-    start_straight(1500);
+    start_straight(900);
     start_keep_angle(180.0);
 
     turn_abs_start_turn(135.0);
@@ -654,5 +654,5 @@ void mode2()
 
 void car_run()
 {
-    go_stop_p9();
+    go_stop_p10();
 }
