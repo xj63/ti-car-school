7803b679e0f43678721db7edf749e330b176c739
zl <2293721550@qq.com>
2024-04-30 16:13:43 +0800

    feat: 参数改进

--- Core/user/motor_control.h
+++ Core/user/motor_control.h
@@ -2,7 +2,7 @@
  * @Author: zl 2293721550@qq.com
  * @Date: 2024-04-03 23:09:00
  * @LastEditors: zl 2293721550@qq.com
- * @LastEditTime: 2024-04-29 12:53:47
+ * @LastEditTime: 2024-04-30 16:00:32
  * @FilePath: \DaChuang\Core\user\motor_control.h
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
